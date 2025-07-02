ccb5bc8b361e5a502aae4fbee0ceffdbc4f08127
Noah <noahskim7@gmail.com>
2025-07-02 16:37:27 -0700

    added index slicing

--- evaluator/evalualor.go
+++ evaluator/evalualor.go
@@ -90,6 +90,19 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 
 		return &object.Array{Elements: elements}
 
+	case *ast.IndexExpression:
+		left := Eval(node.Left, env)
+		if isError(left) {
+			return left
+		}
+
+		index := Eval(node.Index, env)
+		if isError(index) {
+			return index
+		}
+
+		return evalIndexExpression(left, index)
+
 	}
 
 	return nil
@@ -232,6 +245,28 @@ func evalStringInfixExpression(operator string, left, right object.Object) objec
 	leftVal := left.(*object.String).Value
 	rightVal := right.(*object.String).Value
 	return &object.String{Value: leftVal + rightVal}
+}
+
+func evalIndexExpression(left, index object.Object) object.Object {
+	switch {
+	case left.Type() == object.ARRAY_OBJ && index.Type() == object.INTEGER_OBJ:
+		return evalArrayIndexExpression(left, index)
+
+	default:
+		return newError("index operator not supported: %s", left.Type())
+	}
+}
+
+// possible place to implement negative indexes
+func evalArrayIndexExpression(array, index object.Object) object.Object {
+	arrayObject := array.(*object.Array)
+	idx := index.(*object.Integer).Value
+	max := int64(len(arrayObject.Elements) - 1)
+
+	if idx < 0 || idx > max {
+		return NULL
+	}
+	return arrayObject.Elements[idx]
 }
 
 func applyFunction(fn object.Object, args []object.Object) object.Object {
